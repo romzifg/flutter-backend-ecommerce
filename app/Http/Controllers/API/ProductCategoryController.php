2196ade7d78eb6a110f88c1b0a478c1b6c2bfb18
Romzi Farhan <romzifg@gmail.com>
2021-11-10 06:33:53 +0700

    update

---
+++ app/Http/Controllers/API/ProductCategoryController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Models\ProductCategory;
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+
+class ProductCategoryController extends Controller
+{
+    public function all(Request $request) {
+        $id = $request->input('id');
+        $limit = $request->input('limit');
+        $name = $request->input('name');
+        $show_product = $request->input('show_product');
+
+        if($id) {
+            $category = ProductCategory::with(['products'])->find($id);
+
+            if($category) {
+                return ResponseFormatter::success(
+                    $category,
+                    'success get data category'
+                );
+            } else {
+                return ResponseFormatter::error(
+                    null,
+                    'category not found',
+                    404
+                );
+            }
+        }
+
+        $category = ProductCategory::query();
+        if($name) {
+            $category->where('name', 'like', '%' . $name . '%');
+        }
+
+        if($show_product) {
+            $category->with('products');
+        }
+
+        return ResponseFormatter::success(
+            $category->paginate($limit),
+            'success get category'
+        );
+    }
+}
---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Exception;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Fortify\Rules\Password;
+
+class UserController extends Controller
+{
+    public function register(Request $request) {
+        try {
+            $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'username' => ['required', 'string', 'max:255', 'unique:users'],
+                'phone' => ['nullable', 'string', 'max:15'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => ['required', 'string', new Password]
+            ]);
+
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'phone' => $request->phone,
+                'username' => $request->username,
+                'password' => Hash::make($request->password),
+            ]);
+
+            $user = User::where('email', $request->email)->first();
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ],'User Registered');
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $error,
+            ],'Authentication Failed', 500);
+        }
+    }
+}
